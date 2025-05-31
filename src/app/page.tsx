b085c5c3d02299eff82491e5b8240066e6a657f0
Basanta Dahal <dahalbasanta02@gmail.com>
2025-05-31 20:19:54 +0400

    🚑 Fix: Corrected page.tsx with Sidebar and TradingPanel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,14 @@
-// File: src/app/page.tsx
-"use client";
+'use client';
 
-import Sidebar from "./components/Sidebar";
-import TradingPanel from "./components/TradingPanel";
-import OmegaPanel from "./components/OmegaPanel";
-import AlertConsole from "./components/AlertConsole";
-import OrdersFeed from "./components/OrdersFeed";
-import StrategySwitcher from "./components/StrategySwitcher";
-import OmegaChat from "./components/OmegaChat";
-import PerformancePanel from "./components/PerformancePanel";
-import BacktestConsole from "./components/BacktestConsole";
-import AITrainingMonitor from "./components/AITrainingMonitor";
+import Sidebar from './components/Sidebar';
+import TradingPanel from './components/TradingPanel';
 
 export default function Home() {
   return (
-    <main className="bg-black text-white min-h-screen font-sans flex">
+    <main className="flex h-screen w-screen bg-gray-100">
       <Sidebar />
-      <div className="flex-1 p-4 space-y-4 overflow-y-auto">
+      <div className="flex-grow p-4">
         <TradingPanel />
-        <PerformancePanel />
-        <OrdersFeed />
-        <StrategySwitcher />
-        <OmegaPanel />
-        <BacktestConsole />
-        <AlertConsole />
-        <AITrainingMonitor />
-        <OmegaChat />
       </div>
     </main>
   );
